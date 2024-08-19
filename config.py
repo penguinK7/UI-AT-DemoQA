b925a440eb1c46bcd5efb45ec2ff006bb25a566a
Ksenia Klenkova <kllenkova7@gmail.com>
2024-08-19 23:43:03 +0300

    картинка

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+import os
+
+# Определение пути к папке ресурсов относительно текущего расположения
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+RESOURCES_DIR = os.path.join(BASE_DIR, 'resources')
